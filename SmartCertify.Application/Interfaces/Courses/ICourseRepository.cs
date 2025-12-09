e3aa3f23ddb1eb5d74b65ac9cc5c21cde7ddedd4
irumi <irumiabeywickrama@gmail.com>
2025-12-09 22:47:01 +0530

    create interface for Course service

---
+++ SmartCertify.Application/Interfaces/Courses/ICourseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartCertify.Domain.Entities;
+
+namespace SmartCertify.Application.Interfaces.Courses
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GetAllCourses();
+
+        Task<IEnumerable<Course>> GetAllCoursesAsync();
+    }
+}
