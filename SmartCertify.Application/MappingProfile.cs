5bbf8db6ac07604f8a6e12f03941143ba78bf716
irumi <irumiabeywickrama@gmail.com>
2025-12-11 12:16:53 +0530

    create mappings

---
+++ SmartCertify.Application/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using SmartCertify.Application.DTOs;
+using SmartCertify.Domain.Entities;
+
+namespace SmartCertify.Application
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Course, CourseDto>().ReverseMap();
+        }
+    }
+}
