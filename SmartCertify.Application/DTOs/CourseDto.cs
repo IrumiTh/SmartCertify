a4821040e063ce97dd8fdbe2099eee7304085c6a
irumi <irumiabeywickrama@gmail.com>
2025-12-11 12:13:58 +0530

    create dto class for course with define details for course

---
+++ SmartCertify.Application/DTOs/CourseDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCertify.Application.DTOs
+{
+    public class CourseDto
+    {
+        public int CourseId { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+    }
+
+    public class CreateCourseDto
+    {
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+    }
+
+    public class UpdateCourseDto
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public class CourseUploadDescriptionDto
+    {
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
