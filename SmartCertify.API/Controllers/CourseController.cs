04fe1805de646aeeba875a93fc30dd69bd9b957e
irumi <irumiabeywickrama@gmail.com>
2025-12-12 21:46:53 +0530

    create congroller for get all courses

---
+++ SmartCertify.API/Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartCertify.Application.Interfaces.Courses;
+
+namespace SmartCertify.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly ICourseService _service;
+
+        public CourseController(ICourseService service)
+        {
+            this._service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCoursesAsync()
+        {
+            var courses = await _service.GetAllCoursesAsync();
+            return Ok(courses);
+        }
+    }
+}
