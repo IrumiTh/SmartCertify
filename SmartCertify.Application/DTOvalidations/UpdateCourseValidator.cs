6be0e17e0c6b16a8975e3298064520a2110ed7b9
irumi <irumiabeywickrama@gmail.com>
2025-12-13 21:34:48 +0530

    implemleneted update course validator

---
+++ SmartCertify.Application/DTOvalidations/UpdateCourseValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using SmartCertify.Application.DTOs;
+using SmartCertify.Application.Interfaces.Courses;
+
+namespace SmartCertify.Application.DTOvalidations
+{
+    public class UpdateCourseValidator : AbstractValidator<UpdateCourseDto>
+    {
+        public UpdateCourseValidator(ICourseRepository repository)
+        {
+            RuleFor(x => x.Title).NotNull()
+                .NotEmpty()
+                .MaximumLength(100)
+                .MustAsync(async (title, cancellation) =>
+                    title == null || !await repository.IsTitleDuplicateAsync(title))
+                .WithMessage("The course title must be unique. The title you passed does exists");
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .NotNull()
+                .MaximumLength(500);
+        }
+    }
+}
