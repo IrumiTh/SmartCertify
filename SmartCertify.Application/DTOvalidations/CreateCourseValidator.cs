3cea67b582f1c69a83845bf8eb68c2ff36a6cd79
irumi <irumiabeywickrama@gmail.com>
2025-12-13 21:33:57 +0530

    Implemented create course validator

---
+++ SmartCertify.Application/DTOvalidations/CreateCourseValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using SmartCertify.Application.DTOs;
+using SmartCertify.Application.Interfaces.Courses;
+
+namespace SmartCertify.Application.DTOvalidations
+{
+    public class CreateCourseValidator : AbstractValidator<CreateCourseDto>
+    {
+        private readonly ICourseRepository _courseRepository;
+        public CreateCourseValidator(ICourseRepository courseRepository)
+        {
+            RuleFor(x => x.Title).NotEmpty().NotNull().MaximumLength(100)
+                .MustAsync(async (title, cancellation) => !await courseRepository.IsTitleDuplicateAsync(title))
+                .WithMessage("The Course Title Must be unique.");
+            RuleFor(x => x.Description).NotEmpty().NotNull().MaximumLength(500);
+            this._courseRepository = courseRepository;
+        }
+    }
+}
