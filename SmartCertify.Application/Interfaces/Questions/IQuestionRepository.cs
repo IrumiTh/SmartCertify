6f4e069823994994d4decbefb693e5a9b69ffedd
irumi <irumiabeywickrama@gmail.com>
2025-12-14 22:02:15 +0530

    implemented question repository interface

---
+++ SmartCertify.Application/Interfaces/Questions/IQuestionRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartCertify.Domain.Entities;
+
+namespace SmartCertify.Application.Interfaces.Questions
+{
+    public interface IQuestionRepository
+    {
+        Task<IEnumerable<Question>> GetAllQuestionAsync();
+
+        Task<Question?> GetQuestionByIdAsync(int questionId);
+
+
+        Task AddQuestionAsync(Question question);
+
+        Task UpdateQuestionAsync(Question question);
+
+        Task DeleteQuestionAsync(Question question);
+    }
+}
