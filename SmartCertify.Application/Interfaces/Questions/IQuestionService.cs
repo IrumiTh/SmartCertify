3fc6b2aec1693a3c778050272628049509e7fd84
irumi <irumiabeywickrama@gmail.com>
2025-12-17 22:12:59 +0530

    question service interface

---
+++ SmartCertify.Application/Interfaces/Questions/IQuestionService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartCertify.Application.DTOs;
+
+namespace SmartCertify.Application.Interfaces.Questions
+{
+    internal interface IQuestionService
+    {
+        Task<IEnumerable<QuestionDto>> GetAllQuestionAsync();
+
+        Task<QuestionDto?> GetQuestionByIdAsync(int courseId);
+
+        Task AddCourseAsync(CreateCourseDto createCourseDto);
+
+        Task<bool> IsTitleDuplicateAsync(string title);
+
+        Task UpdateCourseAsync(int courseId, UpdateCourseDto updateCourseDto);
+
+        Task DeleteCourseAsync(int courseId);
+
+        Task UpdateDescriptionAsync(int courseId, string descriptionDto);
+    }
+}
