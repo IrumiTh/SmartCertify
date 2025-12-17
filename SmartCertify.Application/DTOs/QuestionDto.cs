b0aa0ba6fca75d4e1724b087ce54ae510601117f
irumi <irumiabeywickrama@gmail.com>
2025-12-17 22:12:16 +0530

    create question dto

---
+++ SmartCertify.Application/DTOs/QuestionDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCertify.Application.DTOs
+{
+    internal class QuestionDto
+    {
+        public int QuestionId { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+    }
+
+    public class CreateCourseDto
+    {
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+    }
+
+    public class UpdateCourseDto
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public class CourseUploadDescriptionDto
+    {
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
