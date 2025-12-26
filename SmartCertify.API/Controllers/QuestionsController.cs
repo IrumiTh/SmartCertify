a3ee2165dc6557ff2044eff00664073d248ba274
irumi <irumiabeywickrama@gmail.com>
2025-12-26 09:40:06 +0530

    implement choices controller

--- SmartCertify.API/Controllers/QuestionsController.cs
+++ SmartCertify.API/Controllers/QuestionsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SmartCertify.Application.DTOs;
+using SmartCertify.Application.Interfaces.Questions;
 
 namespace SmartCertify.API.Controllers
 {
@@ -7,9 +9,53 @@ namespace SmartCertify.API.Controllers
     [ApiController]
     public class QuestionsController : ControllerBase
     {
-        public QuestionsController(IQuestionSe)
+        private readonly IQuestionService _service;
+        public QuestionsController(IQuestionService servive)
         {
-            
+            _service = servive;
         }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<QuestionDto>>> GetAllQuestions()
+        {
+            var questions = await _service.GetAllQuestionsAsync();
+            return Ok(questions);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<QuestionDto>> GetQuestion(int id)
+        {
+            var question = await _service.GetQuestionByIdAsync(id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+            return Ok(question);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateQuestion([FromBody] CreateQuestionDto dto)
+        {
+            await _service.AddQuestionAsync(dto);
+            return CreatedAtAction(nameof(GetQuestion), new { id = dto.CourseId }, dto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateQuestion(int id, [FromBody] UpdateQuestionDto dto)
+        {
+            await _service.UpdateQuestionAsync(id, dto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteQuestion(int id)
+        {
+            await _service.DeleteQuestionAsync(id);
+            return NoContent();
+        }
+
+
+
+
+
     }
 }
