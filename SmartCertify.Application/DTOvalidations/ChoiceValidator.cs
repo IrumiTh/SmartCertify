3104e0eb3efc6b2879902dedf9fb38e69c63b041
irumi <irumiabeywickrama@gmail.com>
2025-12-27 12:21:53 +0530

    choice validator

---
+++ SmartCertify.Application/DTOvalidations/ChoiceValidator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using SmartCertify.Application.DTOs;
+
+namespace SmartCertify.Application.DTOvalidations
+{
+    public class ChoiceValidator : AbstractValidator<CreateChoiceDto>
+    {
+        public ChoiceValidator()
+        {
+            RuleFor(c => c.ChoiceText)
+                .NotEmpty()
+                .WithMessage("Choice text is required.")
+                .MaximumLength(200)
+                .WithMessage("Choice text cannot exceed 200 characters.");
+
+            RuleFor(c => c.QuestionId)
+                .GreaterThan(0)
+                .WithMessage("QuestionId must be greater than 0.");
+        }
+    }
+
+    public class UpdateChoiceValidator : AbstractValidator<UpdateChoiceDto>
+    {
+        public UpdateChoiceValidator()
+        {
+            RuleFor(c => c.ChoiceText)
+                .NotEmpty()
+                .WithMessage("Choice text is required.")
+                .MaximumLength(200)
+                .WithMessage("Choice text cannot exceed 200 characters.");
+        }
+    }
+}
