124785b0ad92bdf862ce624f8a65ea3a2f1c58ec
irumi <irumiabeywickrama@gmail.com>
2025-12-27 12:23:03 +0530

    implement question validator

---
+++ SmartCertify.Application/DTOvalidations/CreateQuestionDtoValidator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using SmartCertify.Application.DTOs;
+using SmartCertify.Application.Interfaces.Courses;
+using SmartCertify.Application.Interfaces.Questions;
+using SmartCertify.Domain.Entities;
+
+namespace SmartCertify.Application.DTOvalidations
+{
+    public class QuestionValidator : AbstractValidator<CreateQuestionDto>
+    {
+        public QuestionValidator()
+        {
+            RuleFor(q => q.QuestionText)
+                .NotEmpty()
+                .WithMessage("Question text is required.")
+                .MaximumLength(500)
+                .WithMessage("Question text cannot exceed 500 characters.");
+
+            RuleFor(q => q.DifficultyLevel)
+                .NotEmpty()
+                .WithMessage("Difficulty level is required.")
+                .MaximumLength(20)
+                .WithMessage("Difficulty level cannot exceed 20 characters.");
+
+            RuleFor(q => q.CourseId)
+                .GreaterThan(0)
+                .WithMessage("CourseId must be greater than 0.");
+        }
+    }
+
+    public class UpdateQuestionValidator : AbstractValidator<UpdateQuestionDto>
+    {
+        public UpdateQuestionValidator()
+        {
+            RuleFor(q => q.QuestionText)
+                .NotEmpty()
+                .WithMessage("Question text is required.")
+                .MaximumLength(500)
+                .WithMessage("Question text cannot exceed 500 characters.");
+
+            RuleFor(q => q.DifficultyLevel)
+                .NotEmpty()
+                .WithMessage("Difficulty level is required.")
+                .MaximumLength(20)
+                .WithMessage("Difficulty level cannot exceed 20 characters.");
+        }
+    }
+}
