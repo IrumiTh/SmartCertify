ef9fb8e5c3a938f506755e5c808228f349463ca2
irumi <irumiabeywickrama@gmail.com>
2026-01-04 22:20:42 +0530

    create bulk DTO

---
+++ SmartCertify.Application/DTOs/BulkCreateChiceDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCertify.Application.DTOs
+{
+    public class BulkCreateChiceDTO
+    {
+        [Required]
+        public int QuestionId { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "At least one choice is required.")]
+        public List<CreateChoiceDto> Choices { get; set; } = new();
+
+    }
+}
